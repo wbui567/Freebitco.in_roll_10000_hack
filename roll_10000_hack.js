91f76b01a2b5d5f02d33148e3c08e524ab7a885e
wbui567 <49976501+wbui567@users.noreply.github.com>
2023-09-26 18:15:20 +0200

    Create roll_10000_hack.js
    to use the script, open console tab into devtool of your favorite browser and paste the code

---
+++ roll_10000_hack.js
@@ -0,0 +1,54 @@
+console["info"]("Activated script");
+Number["prototype"]["Crop"]= 
+function(x)
+{
+	var s=this+ "",a=s["split"](".");
+	a[1]= a[1]|| "";return parseFloat(a[0]+ "."+ a[1]["substring"](0,x))
+};
+var i=true;
+var t=0;
+//var nb=1000;
+var nb=10000;
+setInterval(function()
+	{
+		if($("#free_play_digits")["css"]("display")!= "none")
+			{
+				t++;
+				if(nb> t&& $("#free_play_result")["css"]("display")== "none")
+				//if(nb> t&& $("#free_play_result")["css"]("display")== "true")
+				{
+					$("#multiplier_first_digit")["html"](Math["floor"](Math["random"]()* 10));
+					$("#multiplier_second_digit")["html"](Math["floor"](Math["random"]()* 10));
+					$("#multiplier_third_digit")["html"](Math["floor"](Math["random"]()* 10));
+					$("#multiplier_fourth_digit")["html"](Math["floor"](Math["random"]()* 10));
+					$("#multiplier_fifth_digit")["html"](Math["floor"](Math["random"]()* 10))
+				}
+				else
+				{
+					$("#free_play_first_digit")["html"](1);
+					$("#free_play_second_digit")["html"](0);
+					$("#free_play_third_digit")["html"](0);
+					$("#free_play_fourth_digit")["html"](0);
+					$("#free_play_fifth_digit")["html"](0);
+					$("#winnings")["html"]($(".br_0_0_5_0")[0]["innerHTML"]["split"](" ")[0]);
+
+						if(i)
+						{
+							i= false;
+							var number=parseFloat($("#balance")["html"]())+ parseFloat($(".br_0_0_5_0")[0]["innerHTML"]["split"](" ")[0]);
+							number= number["Crop"](8);
+							localStorage["balance"]= number;
+							$("#balance")["html"](number)
+						}
+
+					}
+
+				}
+
+				if(i== false)
+				{
+					$("#balance")["html"](localStorage["balance"])
+				}
+
+}
+,1)
